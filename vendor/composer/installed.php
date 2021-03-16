825882b9f666d8505b13c9161c5d46167941542a
Mike Hemberger <mike@thestizmedia.com>
2021-03-16 12:39:52 -0400

    update updater

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-develop',
+    'version' => 'dev-develop',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'a92e2b41123787e447f2b8a245657e8c650d380a',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-develop',
+      'version' => 'dev-develop',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'a92e2b41123787e447f2b8a245657e8c650d380a',
+    ),
+    'yahnis-elsts/plugin-update-checker' => 
+    array (
+      'pretty_version' => 'v4.11',
+      'version' => '4.11.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '3155f2d3f1ca5e7ed3f25b256f020e370515af43',
+    ),
+  ),
+);
